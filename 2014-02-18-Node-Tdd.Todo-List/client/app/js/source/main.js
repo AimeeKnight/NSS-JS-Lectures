561dc8cffbd77556e1a2fe5ba39b48174d8eb61d
Aimee Knight <AimeeKnight83@gmail.com>
2014-02-20 09:14:06 -0600

    add ability to dynamically edit priority values

--- 2014-02-18-Node-Tdd.Todo-List/client/app/js/source/main.js
+++ 2014-02-18-Node-Tdd.Todo-List/client/app/js/source/main.js
@@ -12,6 +12,7 @@
     $('#priorities').on('click', '.delBtn',  deletePriority);
     $('#priorities').on('click', '.saveBtn',  updatePriority);
     $('#priorities').on('click', '.name',  editPriority);
+    $('#priorities').on('click', '.value',  editPriorityValue);
   }
 
 ///////////// USERS //////////////
@@ -34,6 +35,7 @@
 
 // ---------- READ ---------- //
   function getPriorities(){
+    $('tbody').empty();
     var url = window.location.origin.replace(/3000/, '4000') + '/priorities';
     //var url = window.location.origin + '4000/users';
     $.getJSON(url, displayPriorities);
@@ -68,9 +70,37 @@
     $(this).replaceWith($name);
   }
 
+  function editPriorityValue(){
+    var $value = $('<td class="update-value-td"></td>');
+    var $input = $('<input class="update-value-input"></input>');
+    $value.append($input);
+    $(this).replaceWith($value);
+  }
+
   function updatePriority(){
-    var name = $(this).parent('.save').siblings('.update-name-td').find('.update-name-input').val();
-    var value = $(this).parent('.save').siblings('.value').text();
+    var name;
+    var value;
+    var newName = $(this).parent('.save').siblings('.update-name-td').find('.update-name-input').val();
+    var oldName = $(this).parent('.save').siblings('.name').text();
+
+    var newValue = $(this).parent('.save').siblings('.update-value-td').find('.update-value-input').val();
+    var oldValue = $(this).parent('.save').siblings('.value').text();
+
+    if (newName){
+      name = newName;
+    }else{
+      name = oldName;
+    }
+
+    if (newValue){
+      value = newValue;
+    }else{
+      value = oldValue;
+    }
+
+    console.log(value);
+    console.log(name);
+
     var priorityId = $(this).parent('.save').parent('tr').data('id');
     var url = window.location.origin.replace(/3000/, '4000') + '/priorities/';
     url += priorityId;
--- 2014-02-18-Node-Tdd.Todo-List/server/app/routes/priorities.js
+++ 2014-02-18-Node-Tdd.Todo-List/server/app/routes/priorities.js
@@ -1,3 +1,4 @@
+/* global exports, global */
 'use strict';
 
 var Priority;
