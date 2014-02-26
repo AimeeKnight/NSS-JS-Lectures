787fa106dfcc8c53cdd35d6afc27a2e1b5d93d71
Aimee Knight <aimeeknight83@gmail.com>
2014-02-25 21:05:46 -0600

    finish flag matching

--- 2014-02-25-Node.Flags-of-the-World/app/static/js/source/main.js
+++ 2014-02-25-Node.Flags-of-the-World/app/static/js/source/main.js
@@ -5,33 +5,41 @@
   $(document).ready(initialize);
   var countryName;
   var flagCode;
+  var i = 120;
+  var $clickedFlag, $clickedCountryName;
 
   function initialize(){
     $(document).foundation();
     $('.flag').click(getCountryCode);
     $('.country').click(getCountryName);
     $('#match').click(lookUpMatch);
-    countDown();
   }
 
+  var timer = setInterval(countDown, 1000);
+
+  
   function countDown(){
-    var i = 120;
-    var $timerH5 = $('#timer').text(i);
-    i--;
-    if (i < 0){
-      $timerH5.text('Time is up!');
+    if (i === 0){
+      clearInterval(timer);
+      $('#timer').text('Time is up!');
     }else{
-      setTimeout(countDown, 1000);
+      i--;
+      $('#timer').text(i);
     }
   }
 
   function getCountryCode(){
+    if ($clickedFlag){
+      $clickedFlag.removeClass('flag-lose');
+      $clickedCountryName.removeClass('alert-box warning radius');
+    }
+    $clickedFlag = $(this);
     flagCode = $(this).attr('class');
     flagCode = flagCode.replace(/flag flag-/, '');
-    $('#flag-code').text(flagCode);
   }
 
   function getCountryName(){
+    $clickedCountryName = $(this);
     countryName = $(this).text();
     $('#country-name').text(countryName);
   }
@@ -42,8 +50,15 @@
   }
 
   function showResult(data){
-    console.log(data.match);
+    if (data.match){
+      $clickedFlag.addClass('flag-win', 1000, 'easeOutBounce' );
+      $clickedCountryName.addClass('alert-box success radius', 1000, 'easeOutBounce' );
+    }else{
+      $clickedFlag.addClass('flag-lose', 1000, 'easeOutBounce' );
+      $clickedCountryName.addClass('alert-box warning radius', 1000, 'easeOutBounce' );
+    }
   }
+
 })();
 
 
