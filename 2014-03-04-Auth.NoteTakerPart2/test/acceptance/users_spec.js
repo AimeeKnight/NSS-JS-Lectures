116484011b16d9c70d9439e28225e977a2f9b718
Aimee Knight <aimeeknight83@gmail.com>
2014-03-04 21:11:32 -0600

    add acceptance tests to user authentication

--- 2014-03-04-Auth.NoteTakerPart2/test/acceptance/users_spec.js
+++ 2014-03-04-Auth.NoteTakerPart2/test/acceptance/users_spec.js
@@ -82,6 +82,7 @@ describe('users', function(){
       .field('password', '1234')
       .end(function(err, res){
         expect(res.status).to.equal(302);
+        expect(res.headers['set-cookie']).to.have.length(1);
         done();
       });
     });
